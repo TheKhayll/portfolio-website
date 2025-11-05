fce6a7416a2fe8156ec490be4548fe0950a6f677
TheKhayll <mikhjnelo@gmail.com>
2025-11-05 17:31:32 +0800

    Added an autoplaying video in the Projects Section

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Parallax for Home Section
+
 let dot = document.getElementById('home-dot');
 let dot2 = document.getElementById('home-dot-2');
 let dot3 = document.getElementById('home-dot-3');
@@ -8,4 +10,23 @@ window.addEventListener('scroll', function(){
     dot.style.bottom = value * -0.03 + '%';
     dot2.style.bottom = value * 0.2 + 'px';
     dot3.style.bottom = value * 0.08 + 'px';
-})
+});
+
+
+// Autoplay Video in Project Section
+
+const projVideo = document.getElementById('projVideo');
+
+// Ensure that the autoplay works even if the browser tries to block it.
+document.addEventListener('DOMContentLoaded', () => {
+    projVideo.muted = true;
+    projVideo.play().catch(() => {
+        // If blocked, try again after the user interaction
+        document.body.addEventListener('click', () => projVideo.play(), { once:true });
+    });
+});
+
+// Prevent interactions
+projVideo.addEventListener('contextmenu', e => e.preventDefault());
+projVideo.addEventListener('click', e => e.preventDefault());
+projVideo.addEventListener('pause', e => e.preventDefault());
